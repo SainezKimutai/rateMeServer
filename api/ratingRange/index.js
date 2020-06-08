2d2c3c95fdcee6328dcf155503f5d07df8f0364f
CynthiaGathoniWainaina <cynthiagathoniwainaina@gmail.com>
2020-06-08 19:01:37 +0300

    --

--- api/ratingRange/index.js
+++ api/ratingRange/index.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post('/create', controller.create);
 router.get('/getAll', controller.getAll);
 router.get('/getOne/:id', controller.getOne);
-router.get('getAllByRangeNumber', controller.getAllByRangeNumber);
+router.get('getRangeByRatingNumber', controller.getRangeByRatingNumber);
 router.put('/update/:id', controller.update);
 router.delete('/delete/:id', controller.delete);
 
