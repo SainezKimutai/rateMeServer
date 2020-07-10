bf7ba86c4de33084630b6d04ae1515c17f1ffa6d
Sainezkimutai <sainezkimutai@gmail.com>
2020-07-10 18:08:46 +0300

    update

--- api/stats/stats.controller.js
+++ api/stats/stats.controller.js
@@ -3,6 +3,10 @@ const customerProfileService = require('../services/customerProfile.service');
 const customerRatingService = require('../services/customerRating.service');
 const industryService = require('../services/industry.service');
 const emojiService = require('../services/emoji.service');
+const responseService = require('../services/response.service');
+const userService = require('../services/user.service');
+const orgBranchService = require('../services/orgBranch.service');
+
 
 
 exports.mostFrqRatedOrgByCustomer = (req, res, next) => {
@@ -216,28 +220,27 @@ exports.totalNumOfRatingByOrg = (req, res, next) => {
 // This function gets the total of ratingNumber below 50 for every business
 exports.totalNumberOfUnpleasantReactionsByOrg = (req,res,next) => {
     customerRatingService.getAllByOrgProfile(req.body.orgProfileId)
-        .then( rsp => {
+        .then(rsp => {
             if(rsp.length > 0) {
-
-
-            }
-
+              let unpleasantRating = rsp.filter((r) => Number(r.ratingNumber) < 50).map(e => e)
+              res.json({totalNumberOfUnpleasantReactions: unpleasantRating.length})
+            } else {res.json({totalNumberOfUnpleasantReactions: 0}) }
             }
         )
+        .catch(err => next(err));
 };
 
 // This function gets the total of ratingNumber from 50 to 100 for every business
 exports.totalNumberOfPleasantReactionsByOrg = (req,res,next) => {
     customerRatingService.getAllByOrgProfile(req.body.orgProfileId)
-        .then( rsp => {
-                if(rsp.length > 0) {
-
-
-                }
-
-            }
-        )
-
+    .then(rsp => {
+        if(rsp.length > 0) {
+          let unpleasantRating = rsp.filter((r) => Number(r.ratingNumber) > 49 ).map(e => e)
+          res.json({totalNumberOfUnpleasantReactions: unpleasantRating.length})
+        } else {res.json({totalNumberOfUnpleasantReactions: 0}) }
+        }
+    )
+    .catch(err => next(err));
 };
 
 
@@ -245,26 +248,87 @@ exports.totalNumberOfPleasantReactionsByOrg = (req,res,next) => {
 // The totals of issues raised are calculated and displayed on a stacked bar chart
 
 exports.topIssuesByOrg = (req,res,next) => {
-
-
-
-
+  customerRatingService.getAllByOrgProfile(req.body.orgProfileId)
+  .then(rsp => {
+      if(rsp.length > 0) {
+        let issues = []
+        rsp.forEach((resp, i, arr) => {
+          resp.questions.forEach((respQuiz, i2, arr2) => {
+            respQuiz.responses.forEach((respRes, i3, arr2) => {
+              responseService.getOne(respRes)
+                .then(respns => {
+                  let obj = {
+                    responseId: respRes,
+                    response: respns.response,
+                    data: resp.updatedAt
+                  }
+                  issues.push(obj)
+                  if (i === arr.length - 1 && i2 === arr2.length - 1 && i3 === arr3.length - 1) {
+                    res.json(issues)
+                  }
+                })
+                .catch(err => next(err));
+            });
+          });
+        });
+      } else {res.json([]) }
+      }
+  )
+  .catch(err => next(err));
 };
+
+
+
+function gettOpRatersByOrg(userArr) {
+  userformated = []
+  userArr.forEach((usr, i, arr) => {
+    customerProfileService.getOne(usr.userProfId)
+        .then(userPfr => {
+            userService.getOne(userPfr.userId)
+                .then(userInfo => {
+                  userformated.push({
+                    email: userInfo.email,
+                    logo: userPrf.logo,
+                    numberOfRating: usr.number
+                  })
+                  if (i === arr.length - 1) {
+                    return userformated;
+                  }
+                })
+                .catch(err => next(err));
+        } )
+        .catch(err => next(err));
+  });
+}
+
 
 // This function gets the top 3 raters for every org.
 // Should contain their email, logo, avg sat rate and most freq selected emoji
 exports.topRatersByOrg = (req,res,next) => {
-
-
+  customerRatingService.getAllByOrgProfile(req.body.orgProfileId)
+  .then(responses => {
+    let userIds = responses.map(e => e.userProfileId )
+    let userArr = []
+    userIds.forEach((itemId, i, arr) => {
+      let myRes = responses.filter((f) => f.userProfileId === itemId).map(e => e);
+      userArr.push({userProfId: itemId, number: myRes.length })
+      if (i === arr.length - 1) {
+          res.json(gettOpRatersByOrg())
+      }
+    })
+  })
+  .catch(err => next(err));
 };
+
+
 
 
 exports.totalNumberOfRatingsByOrgBranch = (req, res, next) => {
     customerRatingService.getAllByOrgBranch(req.body.orgBranchId)
-        .then(rsp => {
-            res.json({totalNumberOfRating: rsp.length});
-        })
-        .catch(err => next(err));
+      .then(rsp => {
+          res.json({totalNumberOfRating: rsp.length});
+      })
+      .catch(err => next(err));
 };
 
 
@@ -328,11 +392,55 @@ exports.mostFreqSelectedEmojiByOrgBranch = (req,res,next) => {
 };
 
 
+
+function getToIssuesByBranch(rsp, branch) {
+  let issues = []
+  rsp.forEach((resp, i, arr) => {
+    resp.questions.forEach((respQuiz, i2, arr2) => {
+      respQuiz.responses.forEach((respRes, i3, arr2) => {
+        responseService.getOne(respRes)
+          .then(respns => {
+            let obj = {
+              branchId: branch._id,
+              branchName: branch.branchName,
+              responseId: respRes,
+              response: respns.response,
+              data: resp.updatedAt
+            }
+            issues.push(obj)
+            if (i === arr.length - 1 && i2 === arr2.length - 1 && i3 === arr3.length - 1) {
+              return issues
+            }
+          })
+          .catch(err => next(err));
+      });
+    });
+  });
+}
+
+
+
 // This function gets the age of the customers who have rated a certain business and gets the average age
 
 exports.topIssuesByOrgBranch = (req,res,next) => {
 
-
+  orgBranchService.getAllByOrgProfileId(req.body.orgProfileId)
+      .then(branches => {
+        if (branches.length > 0) {
+          let AllIssues = []
+          branches.forEach((branch, i, arr) => {
+            customerRatingService.getAllByOrgBranch(branch._id)
+                .then(rsps => {
+                  allIssues.concat(getToIssuesByBranch(rsps, branch))
+                  if (i === arr.length - 1) {
+                    res.json(allIssues)
+                  }
+                 })
+                .catch(err => next(err));
+          });
+        } else { res.json([])}
+      })
+      .catch(err => next(err));
 
 };
 
